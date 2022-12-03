f27c996c2a78041dab2f8a84ec0308d2b259ebf7
Minh Le <minhle2704@gmail.com>
2022-12-03 12:00:05 -0800

    Store user and user orders; Implement login and myorder APIs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 import express from "express";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 import { Low, JSONFile } from "lowdb";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const adapter = new JSONFile("swagData.json");
+const adapter = new JSONFile("db.json");
 const db = new Low(adapter);
+
+// Login
+app.post("/login", async (req, res) => {
+  await db.read();
+
+  const user = db.data.users.find(
+    (user) =>
+      req.body.username === user.username && req.body.password === user.password
+  );
+
+  if (user) {
+    res.send({ role: user.role, id: user.id });
+  } else {
+    res.sendStatus(401);
+  }
+});
 
 // Get all swags (for User & Admin)
 app.get("/swags", async (req, res) => {
@@ -16,8 +33,16 @@ app.get("/swags", async (req, res) => {
   res.send(db.data.swags);
 });
 
+// Get all order (for User & Admin)
+app.post("/my-order", async (req, res) => {
+  await db.read();
+  const user = db.data.users.find((user) => req.body.userId === user.id);
+
+  res.send({ orders: user.orders });
+});
+
 // Add a swag (for Admin)
-app.post("/swags", async (req, res, next) => {
+app.post("/swags", async (req, res) => {
   await db.read();
 
   const createdSwag = {
@@ -32,7 +57,7 @@ app.post("/swags", async (req, res, next) => {
 });
 
 // Edit a swag (for Admin)
-app.post("/swags/:id", async (req, res, next) => {
+app.post("/swags/:id", async (req, res) => {
   await db.read();
 
   const editedSwag = {
@@ -52,7 +77,7 @@ app.post("/swags/:id", async (req, res, next) => {
 });
 
 // Delete a swag (for Admin)
-app.delete("/swags/:id", async (req, res, next) => {
+app.delete("/swags/:id", async (req, res) => {
   await db.read();
 
   db.data.swags = db.data.swags.filter(
@@ -64,12 +89,12 @@ app.delete("/swags/:id", async (req, res, next) => {
   res.sendStatus(200);
 });
 
-// Update swag database after order (for User)
-app.post("/commit-order", async (req, res, next) => {
+// Update swag and user database after order (for User and Admin)
+app.post("/commit-order", async (req, res) => {
   await db.read();
 
   let orderedSwagMap = new Map();
-  for (const swag of req.body) {
+  for (const swag of Object.values(req.body.swagOrders)) {
     orderedSwagMap.set(swag.id, swag);
   }
   db.data.swags = db.data.swags.map((swag) => {
@@ -79,6 +104,17 @@ app.post("/commit-order", async (req, res, next) => {
       : swag;
   });
 
+  let orderInfo = {};
+  orderInfo[uuidv4()] = Object.values(req.body.swagOrders);
+
+  db.data.users = db.data.users.map((user) =>
+    user.id === req.body.userId
+      ? {
+          ...user,
+          orders: [...user.orders, { ...orderInfo }],
+        }
+      : user
+  );
   await db.write();
 
   res.send(db.data.swags);
